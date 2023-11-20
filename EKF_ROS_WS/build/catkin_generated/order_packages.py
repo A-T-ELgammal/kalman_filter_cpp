e3d8450226e31c3d9dc3fdc459bb5501d6815718
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-11-20 21:23:37 +0200

    remove WS

--- EKF_ROS_WS/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ahmed/projects/udacity/Localization/localization_algorithms/EKF_ROS_WS/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ahmed/projects/udacity/GoChaseIt!/devel;/home/ahmed/projects/manipulator_robot/arduinobot_ws/devel;/opt/ros/noetic'.split(';') if '/home/ahmed/projects/udacity/GoChaseIt!/devel;/home/ahmed/projects/manipulator_robot/arduinobot_ws/devel;/opt/ros/noetic' != '' else []
